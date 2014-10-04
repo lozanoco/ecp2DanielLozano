31f404436129cc394991d6ed02b674569e41a6ea
Daniel Lozano <mastermiw@192.168.7.31>
2014-10-04 13:05:24 +0200

    Inicial con pruebas

---
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -0,0 +1,32 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+    private DecimalCollection collection;
+
+    @Before
+    public void before() { 
+        collection = new DecimalCollection();
+        collection.add(2.5);
+        collection.add(3.5);
+    }
+    
+    @Test 
+    public void testSize() { 
+        assertEquals(2, collection.size());  
+    } 
+
+    @Test 
+    public void testHigher() { 
+        assertEquals(3.5, collection.higher(), 0.1);
+    } 
+
+    @Test 
+    public void testSum() { 
+        assertEquals(6, collection.sum(), 0.1);
+    } 
+}
